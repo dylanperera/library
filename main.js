7adfc1a03fbf7054937ec1a6ac6b8984e5a4952c
Dylan Perera <d.perera@mail.utoronto.ca>
2022-07-09 21:03:23 -0400

    Finished the functionality of the discard button

--- main.js
+++ main.js
@@ -16,117 +16,171 @@ function addBook(inventory) {
 function pauseBackground() {}
 
 function newBook(inventory) {
-    createForm();
+  createForm();
 }
 
 //Constructor for the form inputs
-function Input(bookId, inputLabelText, inputPlaceHolder, inputType)
-{
-    this.inputField = document.createElement('input');
-    this.inputLabel = document.createElement('label');
-    this.inputField.id = bookId;
-    this.inputLabel.htmlFor = bookId;
-    if(inputPlaceHolder != 'NA')
-    {
-      this.inputField.placeholder = inputPlaceHolder;
-    }
-    this.inputLabel.textContent = inputLabelText;
-    this.inputField.type = inputType;
-    if(inputType == 'number')
-    {
-      this.inputField.min = 0;
-    }
+function Input(bookId, inputLabelText, inputPlaceHolder, inputType) {
+  this.inputField = document.createElement("input");
+  this.inputLabel = document.createElement("label");
+  this.inputField.id = bookId;
+  this.inputLabel.htmlFor = bookId;
+  if (inputPlaceHolder != "NA") {
+    this.inputField.placeholder = inputPlaceHolder;
+  }
+  this.inputLabel.textContent = inputLabelText;
+  this.inputField.type = inputType;
+  if (inputType == "number") {
+    this.inputField.min = 0;
+  }
 }
 
-function createForm()
-{
+function createForm() {
+  let form = document.createElement("form");
+  form.classList.add("inputForm");
 
-    let form = document.createElement('form');
-    form.classList.add('inputForm');
+  //header element of form
+  let header = document.createElement("h1");
+  header.textContent = "Add New Book";
 
-    //header element of form
-    let header = document.createElement('h1');
-    header.textContent = "Add New Book";
+  //Book Title elements
+  let bookDiv = addBookTitleElement();
 
-    //Book Title elements
-    let bookTitle = new Input('bookTitle', 'Book Title:', '  Percy Jackson and the ...', 'text');
-    let bookDiv = document.createElement('div');
-    bookDiv.appendChild(bookTitle.inputLabel);
-    bookDiv.appendChild(bookTitle.inputField);
+  //Author elements
+  let authorDiv = addAuthorElement();
 
-    //Author elements
-    let author = new Input('author', 'Author:', '  J.K. Rowling', 'text');
-    let authorDiv = document.createElement('div');
-    authorDiv.appendChild(author.inputLabel);
-    authorDiv.appendChild(author.inputField);
-    
-    //Current page elements
-    let currentPage = new Input('currentPage', 'Current Page: ', '  0', 'number');
-    let currentPageDiv = document.createElement('div');
-    currentPageDiv.appendChild(currentPage.inputLabel);
-    currentPageDiv.appendChild(currentPage.inputField);
+  //Current page elements
+  let currentPageDiv = addCurrentPageElement();
 
-    //Total pages elements
-    let totalNumPages = new Input('totalPages', 'Total Pages: ', '  989', 'number');
-    let totalPagesDiv = document.createElement('div');
-    totalPagesDiv.appendChild(totalNumPages.inputLabel);
-    totalPagesDiv.appendChild(totalNumPages.inputField);
-    
-    //Color picker elements
-    let colorPicker = new Input('cardColor', 'Card Color: ', 'NA', 'color')
-    let colorPickerDiv = document.createElement('div');
-    colorPickerDiv.classList.add("colorPicker");
-    colorPickerDiv.appendChild(colorPicker.inputLabel);
-    colorPickerDiv.appendChild(colorPicker.inputField);
+  //Total pages elements
+  let totalPagesDiv = addTotalPagesElement();
 
-    //Buttons elements
-    let addButton = document.createElement('input');
-    addButton.type = 'button';
-    addButton.value = 'Add';
+  //Color picker elements
+  let colorPickerDiv = addColorPickerElement();
 
-    let discardButton = document.createElement('input');
-    discardButton.type = 'button';
-    discardButton.value = 'Discard';
-
-    let buttonsDiv = document.createElement('div');
-    buttonsDiv.classList.add('buttons');
-    buttonsDiv.appendChild(addButton);
-    buttonsDiv.appendChild(discardButton);
-
-    form.appendChild(header);
-    form.appendChild(bookDiv);
-    form.appendChild(authorDiv);
-    form.appendChild(currentPageDiv);
-    form.appendChild(totalPagesDiv);
-    form.appendChild(colorPickerDiv);
-    form.appendChild(buttonsDiv);
-
-    let body = document.querySelector('body');
-
-    let shadeScreen = document.createElement('div');
-    shadeScreen.classList.add('darkenScreen');
+  //Buttons elements
+  let buttonsDiv = addButtons(form);
   
-    body.appendChild(shadeScreen);
-    body.appendChild(form);
+
+  appendChildrenToDoc(
+    form,
+    header,
+    bookDiv,
+    authorDiv,
+    currentPageDiv,
+    totalPagesDiv,
+    colorPickerDiv,
+    buttonsDiv
+  );
+
+  
 }
 
+function addBookTitleElement() {
+  let bookTitle = new Input(
+    "bookTitle",
+    "Book Title:",
+    "  Percy Jackson and the ...",
+    "text"
+  );
+  let bookDiv = document.createElement("div");
+  bookDiv.appendChild(bookTitle.inputLabel);
+  bookDiv.appendChild(bookTitle.inputField);
+  return bookDiv;
+}
+
+function addAuthorElement() {
+  let author = new Input("author", "Author:", "  J.K. Rowling", "text");
+  let authorDiv = document.createElement("div");
+  authorDiv.appendChild(author.inputLabel);
+  authorDiv.appendChild(author.inputField);
+  return authorDiv;
+}
+
+function addCurrentPageElement() {
+  let currentPage = new Input("currentPage", "Current Page: ", "  0", "number");
+  let currentPageDiv = document.createElement("div");
+  currentPageDiv.appendChild(currentPage.inputLabel);
+  currentPageDiv.appendChild(currentPage.inputField);
+  return currentPageDiv;
+}
+
+function addTotalPagesElement() {
+  let totalNumPages = new Input(
+    "totalPages",
+    "Total Pages: ",
+    "  989",
+    "number"
+  );
+
+  let totalPagesDiv = document.createElement("div");
+  totalPagesDiv.appendChild(totalNumPages.inputLabel);
+  totalPagesDiv.appendChild(totalNumPages.inputField);
+  return totalPagesDiv;
+}
+
+function addColorPickerElement()
+{
+  let colorPicker = new Input("cardColor", "Card Color: ", "NA", "color");
+  let colorPickerDiv = document.createElement("div");
+  colorPickerDiv.classList.add("colorPicker");
+  colorPickerDiv.appendChild(colorPicker.inputLabel);
+  colorPickerDiv.appendChild(colorPicker.inputField);
+  return colorPickerDiv;
+}
+
+function addButtons(form)
+{
+  let addButton = document.createElement("input");
+  addButton.type = "button";
+  addButton.value = "Add";
+
+  let discardButton = document.createElement("input");
+  discardButton.type = "button";
+  discardButton.value = "Discard";
+  
+  discardButton.addEventListener('click', () => {
+    let body = document.querySelector('body');
+    body.removeChild(form);
+    let shadeScreen = document.getElementById('darkenScreen');
+    body.removeChild(shadeScreen);
+  });
 
 
 
+  let buttonsDiv = document.createElement("div");
+  buttonsDiv.classList.add("buttons");
+  buttonsDiv.appendChild(addButton);
+  buttonsDiv.appendChild(discardButton);
+  return buttonsDiv;
+}
 
+function appendChildrenToDoc(
+  form,
+  header,
+  bookDiv,
+  authorDiv,
+  currentPageDiv,
+  totalPagesDiv,
+  colorPickerDiv,
+  buttonsDiv
+) {
+  form.appendChild(header);
+  form.appendChild(bookDiv);
+  form.appendChild(authorDiv);
+  form.appendChild(currentPageDiv);
+  form.appendChild(totalPagesDiv);
+  form.appendChild(colorPickerDiv);
+  form.appendChild(buttonsDiv);
 
+  let body = document.querySelector("body");
 
+  let shadeScreen = document.createElement("div");
+  shadeScreen.id = "darkenScreen";
 
-
-
-
-
-
-
-
-
-
-
+  body.appendChild(shadeScreen);
+  body.appendChild(form);
+}
 
 function mergeSort(inputArray) {
   let sortedArray;
